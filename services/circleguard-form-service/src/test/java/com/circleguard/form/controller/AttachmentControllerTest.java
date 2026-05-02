670eb8959ae003b35a7924376f33ca2f4e727747
Juan Casanova <129115242+juancasanov@users.noreply.github.com>
2026-05-02 11:30:34 -0500

    fix: add mockbean for kafka

--- services/circleguard-form-service/src/test/java/com/circleguard/form/controller/AttachmentControllerTest.java
+++ services/circleguard-form-service/src/test/java/com/circleguard/form/controller/AttachmentControllerTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,6 +18,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 class AttachmentControllerTest {
+
+    @MockBean
+    private KafkaTemplate<String, Object> kafkaTemplate;
 
     @Autowired
     private MockMvc mockMvc;
